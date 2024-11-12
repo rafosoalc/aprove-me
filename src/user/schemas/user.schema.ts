bb97eaa199e3e3bb61adc41bf0cdef5537fcc49e
rafosoalc <rafosoalc@hotmail.com>
2024-11-12 19:49:41 +0000

    feat/bcrypt-user-password

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import * as bcrypt from "bcrypt";
 
 export const UserSchema = new mongoose.Schema({
 
@@ -22,5 +23,48 @@ export const UserSchema = new mongoose.Schema({
         default: true
     },
 
-   
+
 }, { timestamps: true })
+
+UserSchema.pre('save', async function (next) {
+    try {
+        if (!this.isModified('password')) {
+            return next();
+        }
+        this['password'] = await bcrypt.hash(this['password'], 10)
+    } catch (error) {
+        return next(error);
+    }
+})
+
+UserSchema.pre('findOneAndUpdate', async function (next) {
+    const updatedFields = this.getUpdate();
+
+    if (updatedFields['password']) {
+        try {
+            const hashedPassword = await bcrypt.hash(updatedFields['password'], 10);
+            updatedFields['password'] = hashedPassword;
+        } catch (err) {
+            return next(err);
+        }
+    }
+
+    next();
+});
+
+UserSchema.pre('updateOne', async function (next) {
+    const updatedFields = this.getUpdate();
+
+    if (updatedFields['password']) {
+        try {
+            const hashedPassword = await bcrypt.hash(updatedFields['password'], 10);
+            updatedFields['password'] = hashedPassword;
+        } catch (err) {
+            return next(err);
+        }
+    }
+
+    next();
+
+
+});
